9e7763a508f1849af2d67771b820d4d067d815f0
Metin de Vreugd <mdevreugd@users.noreply.github.com>
2025-12-11 14:55:28 +0100

    Fix rustup-init script to include Homebrew PATH

--- run_once_06-rustup-init.sh
+++ run_once_06-rustup-init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Set up Homebrew PATH
+if [[ -f /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -f /usr/local/bin/brew ]]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
+
 if [[ ! -d "$HOME/.cargo" ]]; then
     echo "Initializing Rust toolchain..."
     rustup-init -y
